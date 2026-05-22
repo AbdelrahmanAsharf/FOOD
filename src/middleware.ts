4f5a0c2845d952a9bb1b0c97767bb0bcd36f7833
Abdelrahman Ashraf <abdelrahmanasharf58@gmail.com>
2026-05-22 15:17:39 +0300

    fix webhook middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,26 +4,23 @@ import { NextResponse } from "next/server";
 
 const isPublicRoute = createRouteMatcher([
   "/", "/contact", "/menu", "/about", "/sign-in", "/sign-up", "/verify-email", "/cart",
-  "/success", "/cancel", 
-  "/api/stripe/webhook"
+  "/success", "/failure",
+  "/api/webhook",
+  "/api/create-user",
 ]);
 
 export default clerkMiddleware(async (authFn, req) => {
-  const { userId } = await authFn(); // ✅ استخدم await
-
-  //const url = req.nextUrl;
+  const { userId } = await authFn();
 
   if (!userId && !isPublicRoute(req)) {
     return NextResponse.redirect(new URL("/sign-in", req.url));
   }
-
-
 
   return NextResponse.next();
 });
 
 export const config = {
   matcher: [
-    "/((?!_next/static|_next/image|favicon.ico|.*\\.png$|.*\\.jpg$|.*\\.jpeg$|.*\\.svg$|.*\\.webp$|api/stripe/webhook).*)"
+    "/((?!_next/static|_next/image|favicon.ico|.*\\.png$|.*\\.jpg$|.*\\.jpeg$|.*\\.svg$|.*\\.webp$|api/webhook).*)"
   ],
 };
